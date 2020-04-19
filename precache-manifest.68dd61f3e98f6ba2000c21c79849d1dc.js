96c50a5b98887392ac4641685ff6d4940cc027ef
Quy Tran Xuan Vu <quy.txv@gmail.com>
2020-04-19 13:14:53 +0700

    Updates

---
+++ precache-manifest.68dd61f3e98f6ba2000c21c79849d1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66293df0cd4df02b791dd1654200619",
+    "url": "/calculator-functional/index.html"
+  },
+  {
+    "revision": "d793030b0e99e1d69c8c",
+    "url": "/calculator-functional/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "472ef50ed8cddf10ea73",
+    "url": "/calculator-functional/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "d793030b0e99e1d69c8c",
+    "url": "/calculator-functional/static/js/2.2cff834f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-functional/static/js/2.2cff834f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472ef50ed8cddf10ea73",
+    "url": "/calculator-functional/static/js/main.c48b138f.chunk.js"
+  },
+  {
+    "revision": "9dfe5145e8733651468c",
+    "url": "/calculator-functional/static/js/runtime-main.6719fba6.js"
+  }
+]);
